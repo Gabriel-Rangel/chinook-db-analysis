f8068ff15cba3ba54f4030a2599d77036d3c4d56
Gabriel Souza Rangel <gsrangel6@gmail.com>
2022-08-04 21:25:25 -0300

    add comments

--- dw_chinook/ddl/dim_calendar.sql
+++ dw_chinook/ddl/dim_calendar.sql
@@ -1,3 +1,4 @@
+-- dw_chinook is the collection of fact and dimensions tables where the datawarehouse is logical organized.
 CREATE TABLE dw_chinook.dim_calendar AS
 SELECT   d                     AS "date"
        , extract(day FROM d)   AS "day" 
@@ -6,5 +7,6 @@ SELECT   d                     AS "date"
        , to_char(d, 'Day')     AS week_day 
 FROM
 (
-	SELECT  cast(generate_series(date '2017-01-01',date '2020-12-31','1 day') AS date) AS d
+	-- interval of range of dates based on max and min dates of orginal transactional database
+	SELECT  cast(generate_series(date '2017-01-01',date '2020-12-31','1 day') AS date) AS d 
 ) base
