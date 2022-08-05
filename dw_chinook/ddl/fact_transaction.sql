3f053363073c69e659895f55759cc3f9f2640937
Gabriel Souza Rangel <gsrangel6@gmail.com>
2022-08-04 21:35:05 -0300

    add comments

--- dw_chinook/ddl/fact_transaction.sql
+++ dw_chinook/ddl/fact_transaction.sql
@@ -1,10 +1,14 @@
+-- dw_chinook is the collection of fact and dimensions tables where the datawarehouse is logical organized.
+-- chinook is the collection of tables that contains the original transactional database.
 CREATE TABLE dw_chinook.fact_transaction AS
 SELECT  invoice_line.invoice_line_id
        ,invoice_line.invoice_id
        ,track_id
        ,unit_price
        ,quantity
+       -- the column total in this table represents the total of the invoice line and not the total of an invoice
        ,unit_price*quantity AS total
+       -- to get the total of an invoice it is needed to sum the column total for every same invoice_id
        ,invoice.customer_id
        ,support_rep_id      AS employee_id
        ,CAST(invoice_date AS DATE) AS invoice_date
