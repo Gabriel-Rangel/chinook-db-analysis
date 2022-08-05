0acd972cda1f18b14295397cafebdd3a3ea7cd8a
Gabriel Souza Rangel <gsrangel6@gmail.com>
2022-08-04 21:30:14 -0300

    add comments

--- dw_chinook/ddl/dim_track.sql
+++ dw_chinook/ddl/dim_track.sql
@@ -1,14 +1,16 @@
+-- dw_chinook is the collection of fact and dimensions tables where the datawarehouse is logical organized.
+-- chinook is the collection of tables that contains the original transactional database.
 CREATE TABLE dw_chinook.dim_track AS
 SELECT  track.track_id
-       ,track.name      AS track
-       ,media_type.name AS media_type
-       ,genre.name      AS genre
-       ,album.title     AS album
-       ,artist.name     AS artist
-       ,playlist.name   AS playlist
-       ,composer
-       ,milliseconds
-       ,bytes
+       ,track.name      AS track -- name of track
+       ,media_type.name AS media_type -- name of media type
+       ,genre.name      AS genre -- name of genre
+       ,album.title     AS album -- name of album
+       ,artist.name     AS artist -- name of artist
+       ,playlist.name   AS playlist -- name of playlist
+       ,track.composer
+       ,track.milliseconds
+       ,track.bytes
 FROM chinook.track
 INNER JOIN chinook.media_type
 ON media_type.media_type_id = track.media_type_id
